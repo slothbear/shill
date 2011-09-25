b7d6eaf4c25856dfa31f4929af858f7ea350d135
Paul Morganthall <paul@morganthall.com>
2011-09-25 01:08:36 -0400

    Get rid of duck punch for sorted hashes.
    1.9.2 does it for us.

--- make_yaml.rb
+++ make_yaml.rb
@@ -3,26 +3,6 @@ require 'yaml'
 LSL_VERSION = '1.23.4'
 UPDATE_NOTICE = "LSL keywords updated #{Time.new.strftime("%d %b %Y")} for LSL #{LSL_VERSION} by http://adammarker.org/shill"
 
-# duck punch from http://blade.nagaokaut.ac.jp/cgi-bin/scat.rb/ruby/ruby-talk/180768?help-en
-class Hash
-  def to_yaml( opts = {} )
-    YAML::quick_emit( object_id, opts ) do |out|
-      out.map( taguri, to_yaml_style ) do |map|
-        sorted_keys = keys
-        sorted_keys = begin
-          sorted_keys.sort
-        rescue
-          sorted_keys.sort_by {|k| k.to_s} rescue sorted_keys
-        end
-
-        sorted_keys.each do |k|
-          map.add( k, fetch(k) )
-        end
-      end
-    end
-  end
-end
-
 # remove quotes from beginning and end of the item.
 def dequote(string)
   string.strip.gsub(/\A"(.*)"\z/m, '\1')
